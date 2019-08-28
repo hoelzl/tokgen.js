79ba3a40dc1a7eca5e1e34c798d3776a194e3fe8
Matthias Hölzl <tc@xantira.com>
2019-08-28 09:01:11 +0200

    Dim the Token screen while no token is available

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Dimmer, Loader } from "semantic-ui-react";
 
 const Home = props => {
   return (
     <div>
+      <Dimmer active={!props.token}>
+        <Loader active={!props.token}>Generating a token</Loader>
+      </Dimmer>
       <p>Thank you for using TokGen!</p>
       {props.token ? (
         <p>The generated token is: {props.token}</p>
       ) : (
-        <div className="ui spinner">
-          <p>Please stand by while the token is generated.</p>
-        </div>
+        <p>Please stand by while the token is generated.</p>
       )}
       <Link to="/data">
         <button className="ui button fluid" onClick={props.clearToken}>
